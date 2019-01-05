3ced845dbcb4b758ab22e3ef824bad32b42d049c
andy <andy.lv.vip@hotmail.com>
2019-01-05 22:28:26 +0800

    add oauth2...

--- licensing/src/main/java/aspire/demo/licensing/LicensingApplication.java
+++ licensing/src/main/java/aspire/demo/licensing/LicensingApplication.java
@@ -7,18 +7,24 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RefreshScope
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableResourceServer
 public class LicensingApplication {
 
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(OAuth2ProtectedResourceDetails details,
+                                     OAuth2ClientContext oauth2ClientContext) {
+        return new OAuth2RestTemplate(details, oauth2ClientContext);
     }
 
     public static void main(String[] args) {
--- zuul-server/src/main/java/aspire/demo/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/aspire/demo/zuulserver/ZuulServerApplication.java
@@ -2,7 +2,6 @@ package aspire.demo.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
