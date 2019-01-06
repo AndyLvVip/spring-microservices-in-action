89dd04f15a269186bdc4d994fa5dbc6c1bdb550a
andy <andy.lv.vip@hotmail.com>
2019-01-06 16:14:03 +0800

    add zipkin...

--- organization/src/main/java/aspire/demo/organization/OrganizationApplication.java
+++ organization/src/main/java/aspire/demo/organization/OrganizationApplication.java
@@ -1,6 +1,7 @@
 package aspire.demo.organization;
 
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
